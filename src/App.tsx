f38ee951887e2c53e7ff5f1f964f7fa869d07185
Josh Sikes <josh.sikes@ramseysolutions.com>
2024-01-30 09:07:26 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-900">
+        <h1 className="text-white text-center">Hello Boyos</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
