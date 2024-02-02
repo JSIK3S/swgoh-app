75cf832b954bdf78402ffed3028cac6261178588
Josh Sikes <josh.sikes@ramseysolutions.com>
2024-02-02 08:09:45 -0600

    adds firebase and additional structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <div className="bg-red-900">
-        <h1 className="text-white text-center">Hello Boyos</h1>
+      <div className="bg-red-900 h-[1000px] flex items-center justify-center">
+        <h1 className="text-white text-center text-3xl">Hello Boyos</h1>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZEHV3QJ7tRZJcQAKYzRkBzdNPxJYl52A",
+  authDomain: "jarjarspoolservice.firebaseapp.com",
+  projectId: "jarjarspoolservice",
+  storageBucket: "jarjarspoolservice.appspot.com",
+  messagingSenderId: "38435514520",
+  appId: "1:38435514520:web:61ca9b19a374a2e63e88ed",
+};
+
+// Initialize Firebase
+// @ts-ignore
+const app = initializeApp(firebaseConfig);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
